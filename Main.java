35afba1a53bff1c4ec5fac464b5e7df8b53bae38
avinash-rana <avinash-rana@users.noreply.github.com>
2019-12-17 16:19:26 +0530

    Not avialable
    see the code fdr better understand.

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "this";
+        Scanner sc=new Scanner(System.in);
+        Scanner sc1=new Scanner(System.in);
+        int i=sc.nextInt();
+        double d=sc.nextDouble();
+        String s2=sc1.nextLine();
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s1+" "+s2);
+
+    }
+}
